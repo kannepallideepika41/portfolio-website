b77e7abb7edaeadb4079d7e1e62a284a358cac11
Deepika <kannepallideepika41@gmail.com>
2026-05-16 12:44:31 +0530

    Enhanced contact form and project animations

--- script.js
+++ script.js
@@ -2,13 +2,17 @@ function validateForm(){
 
     let name = document.getElementById("name").value;
     let email = document.getElementById("email").value;
+    let message = document.getElementById("message").value;
 
-    if(name === "" || email === ""){
-        alert("Fill all fields");
+    if(name === "" || email === "" || message === ""){
+
+        alert("Please fill all fields");
+
         return false;
     }
 
-    alert("Form Submitted Successfully");
+    alert("Message Sent Successfully!");
+
     return true;
 }
 
